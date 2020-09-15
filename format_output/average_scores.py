e0137761faec894e6d2abd9809595163d82a6ccf
djbittner <djbittner@dmacc.edu>
2020-09-15 18:51:54 -0500

    test_average fail

---
+++ format_output/average_scores.py
@@ -0,0 +1,18 @@
+"""
+Author:  Derek Bittner
+Program:  average_scores.py
+
+program that takes average of three scores out of 100
+
+"""
+def average():
+    score1= input("Enter your first grade:")
+    score2= input("Enter your second grade:")
+    score3= input("Enter your third grade:")
+    return(int(score1)+ int(score2)+ int(score3))/3
+
+if __name__ == '__main__':
+
+score1 = input("Enter your first name:")
+score2 = input("Enter your Last name:")
+score3 = input("
